a75050358173a4d0f9d6dd6fd824bf1bac578b45
pravshot <praveenkalva@icloud.com>
2023-09-03 20:02:59 -0400

    updated the database script

--- ml_model/update_script.py
+++ ml_model/update_script.py
@@ -6,13 +6,11 @@ from requests.adapters import HTTPAdapter, Retry
 from newsapi import NewsApiClient
 import pandas as pd
 
-POPULAR_NEWS_SOURCES = [
-    "bbc-news",
-    "cnn",
-    "the-verge",
-    "the-wall-street-journal",
-    "the-washington-post",
-]
+# load the model
+model = keras.models.load_model(
+    "./news_sentiment_model"
+)
+
 # preprocess text before putting into model
 def preprocess(text):
     # lowercase
@@ -26,39 +24,18 @@ def preprocess(text):
     return text
 
 
-# load the model
-model = keras.models.load_model(
-    "/Users/praveenkalva/Desktop/promising_news/ml_model/news_sentiment_model"
-)
-
-URL = "https://promising-news-api.herokuapp.com/news"
-
 # get the latest news
-newsapi = NewsApiClient(api_key="06cda5faf3cf4396b3d4daf4a8540669")
-# top_headlines = newsapi.get_top_headlines(language='en', country='us', page_size=100, page=1)
-# print(len(top_headlines['articles']))
-# print(top_headlines['totalResults'])
+newsapi = NewsApiClient(api_key="")
+top_headlines = newsapi.get_top_headlines(language='en', country='us', page_size=100, page=1)
 
-# get todays date in iso format
-today = pd.Timestamp.today().isoformat().split("T")[0]
-
-all_articles = newsapi.get_everything(
-    from_param=today,
-    to=today,
-    sources=",".join(POPULAR_NEWS_SOURCES),
-    language="en",
-    sort_by="popularity",
-    page=1,
-    page_size=100,
-)
-
+API_URL = "https://promising-news.uc.r.appspot.com/news"
 
 s = requests.Session()
 retries = Retry(total=2, backoff_factor=0.1)
-s.mount("http://", HTTPAdapter(max_retries=retries))
+s.mount("https://", HTTPAdapter(max_retries=retries))
 
 request_hist = pd.DataFrame(columns=["article", "status_code"])
-for article in all_articles["articles"]:
+for article in top_headlines["articles"]:
     title = article["title"]
     score = model.predict(preprocess([title]))[0][0]
     if score > 0.5:
@@ -70,10 +47,10 @@ for article in all_articles["articles"]:
             date=article["publishedAt"],
             url=article["url"],
             image_url=article["urlToImage"],
-            publication=article["source"]["id"],
+            publication=article["source"]["name"],
             positivity_score=score,
         )
-        r = s.post(URL, data=payload)
+        r = s.post(API_URL, data=payload)
         request_hist.loc[len(request_hist.index)] = [title, r.status_code]
 
 print(request_hist["status_code"].value_counts())
